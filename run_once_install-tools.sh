c1eafd254084eea8fab728ffe5d59a76ffe0e29a
Oscar Alias <oscar.alias@gmail.com>
2022-10-21 13:00:22 -0600

    Update the commands to get remote and local verions

--- run_once_install-tools.sh
+++ run_once_install-tools.sh
@@ -45,21 +45,21 @@ app_binaries[roxctl]=roxctl
 
 # declare the method to get the latest release of the application
 declare -A remote_versions
-remote_versions[oc]="curl -s https://mirror.openshift.com/pub/openshift-v4/clients/ocp/latest/release.txt | awk '/Version:/ { print \$NF }'"
+remote_versions[oc]="curl -sL https://mirror.openshift.com/pub/openshift-v4/clients/ocp/latest/release.txt | awk '/Version:/ { print \$NF }'"
 remote_versions[helm]="curl -sL https://github.com/helm/helm/releases/latest | sed -E -n 's/.*tag\/(v[0-9\.]+).*/\1/p' | tail -n1"
-remote_versions[krew]="curl -s https://github.com/kubernetes-sigs/krew/releases/latest | sed -E -n 's/.*tag\/(v[0-9\.]+).*/\1/p'"
+remote_versions[krew]="curl -sL https://github.com/kubernetes-sigs/krew/releases/latest | grep -m1 'kubernetes-sigs/krew/releases/tag' | sed -E -n 's/.*tag\/(v[0-9\.]+).*/\1/p'"
 remote_versions[tekton]="curl -sL https://github.com/tektoncd/cli/releases/latest | sed -E -n 's/.*tag\/v([0-9\.]+).*/\1/p' | tail -n1"
 remote_versions[kustomize]="curl -s https://api.github.com/repos/kubernetes-sigs/kustomize/releases | grep browser_download.*linux_amd64 | cut -d '\"' -f 4 | sort -V | tail -n 1 | sed -E -n 's/.*kustomize\/(v[0-9\.]+).*/\1/p'"
-remote_versions[knative]="curl -s https://github.com/knative/client/releases/latest | sed -E -n 's/.*knative-(v[0-9\.]+).*/\1/p'"
-remote_versions[argocd]="curl -s https://github.com/argoproj/argo-cd/releases/latest | sed -E -n 's/.*tag\/(v[0-9\.]+).*/\1/p'"
-remote_versions[roxctl]="curl -s  https://mirror.openshift.com/pub/rhacs/assets/latest/bin/Linux/sha256sum.txt | cut -d' ' -f1"
+remote_versions[knative]="curl -sL https://github.com/knative/client/releases/latest | grep -m1 'knative/client/releases/tag' | sed -E -n 's/.*knative-(v[0-9\.]+).*/\1/p'"
+remote_versions[argocd]="curl -sL https://github.com/argoproj/argo-cd/releases/latest | grep -m1 'argoproj/argo-cd/releases/tag' | sed -E -n 's/.*tag\/(v[0-9\.]+).*/\1/p'"
+remote_versions[roxctl]="curl -sL  https://mirror.openshift.com/pub/rhacs/assets/latest/bin/Linux/sha256sum.txt | cut -d' ' -f1"
 
 # declare the method to get the current version of the application
 declare -A local_versions
-local_versions[oc]="oc version | awk '/Version/ { print \$NF }'"
+local_versions[oc]="oc version | awk '/Client Version/ { print \$NF }'"
 local_versions[helm]="helm version | sed -E -n 's/.*Version:.(v[0-9\.]+)..*/\1/p'"
 local_versions[krew]="kubectl-krew version | awk '/GitTag/ { print \$NF }'"
-local_versions[tekton]="tkn version | awk '/version/ { print \$NF }'"
+local_versions[tekton]="tkn version | awk '/Client version/ { print \$NF }'"
 local_versions[kustomize]="kustomize version | sed -E -n 's/.*kustomize\/(v[0-9\.]+)\s.*/\1/p'"
 local_versions[knative]="kn version | awk '/Version/ { print \$NF }'"
 local_versions[argocd]="argocd version 2>/dev/null| sed -E -n 's/argocd:\s+(v[0-9\.]+).*/\1/p'"
@@ -75,7 +75,6 @@ install_methods[kustomize]="curl -sL https://github.com/kubernetes-sigs/kustomiz
 install_methods[knative]="curl -sL https://github.com/knative/client/releases/latest/download/kn-linux-amd64 -o ${LOCAL_BIN}/kn ; chmod +x ${LOCAL_BIN}/kn"
 install_methods[argocd]="curl -sL https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64 -o ${LOCAL_BIN}/argocd ; chmod +x ${LOCAL_BIN}/argocd"
 install_methods[roxctl]="curl -sL https://mirror.openshift.com/pub/rhacs/assets/latest/bin/Linux/roxctl -o ${LOCAL_BIN}/roxctl ; chmod +x ${LOCAL_BIN}/roxctl"
-
 
 ##  Array with the list of tools to install
 tools=(
